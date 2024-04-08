64a5241fc531f665773031df3cfc7c20162f5de1
pasure <56163008+pasure@users.noreply.github.com>
2024-04-08 19:17:45 +0700

    fix: rename path

--- Project 2 : Visualizing Air Pollution (PM 2.5) on a Map/UPDATE_Geom.sql
+++
@@ -1,8 +0,0 @@
-USE [Project_PM25]
-GO
-
-UPDATE [dbo].[AirPollutionPM25]    
-   SET Geom = geometry:: STGeomFromText('POINT('+convert(varchar(20),longitude)+' '+convert(varchar(20),latitude)+')',4326);
-GO
-
-
